abef858653aa5caf90024b1e51f2b0d6254df9dd
vedkerkar10 <113177694+vedkerkar10@users.noreply.github.com>
2024-07-14 19:30:30 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,35 +1,24 @@
-import spacy
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import warnings
-import os
+import json
+import ollama
 
-# Suppress specific warnings
-warnings.filterwarnings("ignore", category=UserWarning, module='transformers')
-
-# Load the trained model
-model_path = os.path.abspath('ner_model')
-nlp = spacy.load(model_path)
-
-# Initialize Flask app
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
-
+CORS(app)
 
 @app.route('/extract', methods=['POST'])
-def extract_entities():
-    data = request.get_json()
-    text = data.get('text', '')
-    doc = nlp(text)
+def extract():
+    data = request.json
+    inp = data['text']
+    stream = ollama.chat(model='gemma2', messages=[
+        {
+            'role': 'user',
+            'content': f'extract all the attributes as a json obj, do not nest more than 1 layer deep, only give me the json, heres the text: {inp}',
+        },
+    ])
 
-    # Extract entities and format them
-    # for ent in doc.ents:
-    #     print(f"Text:{ent.text}, Label:{ent.label_}")
-    entities = {ent.label_: ent.text for ent in doc.ents}
-
-    print(entities)
-    return jsonify(entities)
-
+    y = json.loads(stream['message']['content'].replace("json", "").replace("", ""))
+    return jsonify(y)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, port=5000)

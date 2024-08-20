de7a9139bc913f4b1418e669fc7e4cd9e5db19bc
vedkerkar10 <113177694+vedkerkar10@users.noreply.github.com>
2024-08-20 22:11:51 +0530

    aa

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
@@ -8,6 +8,14 @@ import Main from "./components/Main";
 
 export default function Home() {
   const [result, setResult] = useState({});
+  const [domain, setDomain] = useState("");
+
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_DOMAIN) {
+      setDomain(process.env.NEXT_PUBLIC_DOMAIN);
+    }
+  },
+  []);
 
   const handleTextSubmit = async (text: string) => {
     try {
@@ -31,10 +39,14 @@ export default function Home() {
     }
   };
 
+  
+  // const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
+  // console.log(DOMAIN);
+
   const handleJobSearch = async (query: any) => {
     try {
       const response = await axios.post(
-        `${process.env.DOMAIN}/api/jobs`,
+        `${domain}/api/jobs`,
         query
       );
       // Store jobs in local storage

3b40ca2b64c8634b1a4af3c37c57f7ba2a02adea
vedkerkar10 <113177694+vedkerkar10@users.noreply.github.com>
2024-08-20 23:02:08 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/(.*)",
+                headers: [
+                    {
+                      key: 'Access-Control-Allow-Origin',
+                      value: '*', // Replace with your allowed origin
+                    },
+                    {
+                      key: 'Access-Control-Allow-Methods',
+                      value: 'GET, POST, OPTIONS',
+                    },
+                    {
+                      key: 'Access-Control-Allow-Headers',
+                      value: 'X-Requested-With, Content-Type',
+                    },
+                  ],
+
+            }
+        ]
+    }
+};
 
 export default nextConfig;
